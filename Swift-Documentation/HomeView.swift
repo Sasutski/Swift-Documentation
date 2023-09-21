a8a2e3a381f68a394d3e3bd9dcda8ae8297ac224
James Tan <tan_yu_kai_james@s2022.ssts.edu.sg>
2023-09-21 17:09:38 +0800

    Tested out SwiftPersistence
    SwiftPersistence works

--- Swift-Documentation/HomeView.swift
+++ Swift-Documentation/HomeView.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import SwiftPersistence
 
 struct HomeView: View {
+    @Persistent("text") var text = "Hello World"
+    
     var body: some View {
         ZStack {
             Color.black
@@ -18,6 +21,19 @@ struct HomeView: View {
                     .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                     .padding(20.0)
                 Spacer()
+                Text(text)
+                
+                Spacer()
+                Button {
+                    text = "Samuel"
+                } label: {
+                    Text("Samuel")
+                }
+                Button {
+                    text = "Zhe Hao"
+                } label: {
+                    Text("Zhe Hao")
+                }
             }
             .foregroundStyle(.white)
         }
