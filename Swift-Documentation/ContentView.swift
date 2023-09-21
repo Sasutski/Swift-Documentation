34318826cb989f0c9940321aeee7fcb1c17472ce
James Tan <tan_yu_kai_james@s2022.ssts.edu.sg>
2023-09-21 12:35:47 +0800

    Initial Commit

---
+++ Swift-Documentation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift-Documentation
+//
+//  Created by Sasutski on 21/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift-Documentation/Swift_DocumentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_DocumentationApp.swift
+//  Swift-Documentation
+//
+//  Created by Sasutski on 21/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_DocumentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
