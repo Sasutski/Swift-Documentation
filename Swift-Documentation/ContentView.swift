6ff3d50dac85be5de30abc0269a3e622aebadf11
Vimaljustchill <nagarajan_vimal_balaji@s2022.ssts.edu.sg>
2023-09-22 08:59:08 +0800

    Update ContentView

--- Swift-Documentation/ContentView.swift
+++ Swift-Documentation/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Welcome to MVCJ!")
+            Text("Welcome to Samuel!")
         }
         .padding()
     }
